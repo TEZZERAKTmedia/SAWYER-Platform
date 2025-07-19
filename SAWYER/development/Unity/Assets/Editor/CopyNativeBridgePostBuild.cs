e3ff17cc9678c1869117aa3edba5c44bfcc7fab7
Trentyn Nicholas <trentynnicholas@Trentyns-MacBook-Pro.local>
2025-07-18 21:52:52 -0600

    Added automated targeting so that the NativeBridge logic that we are moving from the unity project > the React Native Embedded hybrid project correctly points to the Unity-iPhone.xcodeproj

--- SAWYER/development/Unity/Assets/Editor/CopyNativeBridgePostBuild.cs
+++ SAWYER/development/Unity/Assets/Editor/CopyNativeBridgePostBuild.cs
@@ -1,3 +1,7 @@
+#if UNITY_IOS
+using UnityEditor.iOS.Xcode;
+#endif
+
 using UnityEditor;
 using UnityEditor.Callbacks;
 using System.IO;
@@ -28,5 +32,35 @@ public class CopyNativeBridgePostBuild
         }
 
         Debug.Log("[NativeBridge] ✅ Copied Swift bridge files to Xcode iOS project.");
+
+        string projPath = PBXProject.GetPBXProjectPath(pathToBuiltProject);
+        PBXProject proj = new PBXProject();
+        proj.ReadFromFile(projPath);
+
+#if UNITY_2019_3_OR_NEWER
+        string targetGuid = proj.GetUnityMainTargetGuid();
+        string frameworkGuid = proj.GetUnityFrameworkTargetGuid();
+#else 
+        string targetGuid = proj.TargetGuidByName("Unity-iPhone");
+        string frameworkGuid = targetGuid;
+#endif 
+        foreach (var file in Directory.GetFiles(targetBridgePath, "*.swift"))
+        {
+            var relativePath = "NativeBridge/" + Path.GetFileName(file);
+            var fileGuid = proj.AddFile(file, relativePath, PBXSourceTree.Source);
+            proj.AddFileToBuild(frameworkGuid, fileGuid);
+        }
+
+        proj.SetBuildProperty(frameworkGuid, "SWIFT_VERSION", "5.0");
+        proj.SetBuildProperty(frameworkGuid, "ENABLE_BITCODE", "NO");
+        proj.SetBuildProperty(frameworkGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
+        proj.SetBuildProperty(frameworkGuid, "DEFINES_MODULE", "YES");
+
+
+        proj.WriteToFile(projPath);
+
+        Debug.Log("[NativeBridge] Linked Swift bridge files into Unity-iPhone.xcodeproj.");
+
+        
     }
 }
